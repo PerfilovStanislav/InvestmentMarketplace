c61ab5801d87e0e349b2b5f098e83da1cca7d68d
PerfilovStanislav <stanislav.perfilov@gmail.com>
2019-08-28 22:29:22 +0300

    Global

--- SQL/sperfilov_public_user_params.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.user_params
-(
-    user_id integer NOT NULL,
-    lang_id smallint
-);
-INSERT INTO public.user_params (user_id, lang_id) VALUES (1, 219);
-INSERT INTO public.user_params (user_id, lang_id) VALUES (32, 317);
-INSERT INTO public.user_params (user_id, lang_id) VALUES (33, null);
--- SQL/sperfilov_public_user_status.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.user_status
-(
-    id smallint NOT NULL,
-    name varchar(50) NOT NULL
-);
-INSERT INTO public.user_status (id, name) VALUES (1, 'need confirm');
-INSERT INTO public.user_status (id, name) VALUES (2, 'registered');
-INSERT INTO public.user_status (id, name) VALUES (3, 'super admin');

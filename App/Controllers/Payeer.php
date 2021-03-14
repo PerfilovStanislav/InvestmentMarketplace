49725a88854b0c1e52fb5034949009e37b36b196
PerfilovStanislav <stanislav.perfilov@gmail.com>
2021-03-14 22:00:11 +0300

    -

--- App/Controllers/Payeer.php
+++ App/Controllers/Payeer.php
@@ -27,6 +27,45 @@ class Payeer extends Controller {
     }
 
     public function status(array $data): Output {
+        \Output()->disableLayout();
+
+        if (!in_array($_SERVER['REMOTE_ADDR'], ['185.71.65.92', '185.71.65.189', '149.202.17.210'])) {
+            return Output();
+        }
+
+        if (isset($data['m_operation_id']) && isset($data['m_sign'])) {
+            $m_key = \Config::PAYEER_SECRET_KEY;
+
+            $arHash = [
+                $data['m_operation_id'],
+                $data['m_operation_ps'],
+                $data['m_operation_date'],
+                $data['m_operation_pay_date'],
+                $data['m_shop'],
+                $data['m_orderid'],
+                $data['m_amount'],
+                $data['m_curr'],
+                $data['m_desc'],
+                $data['m_status']
+            ];
+
+            if (isset($data['m_params'])) {
+                $arHash[] = $data['m_params'];
+            }
+
+            $arHash[] = $m_key;
+
+            $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
+
+            if ($data['m_sign'] === $sign_hash && $data['m_status'] === 'success') {
+                ob_end_clean();
+                exit($data['m_orderid'] . '|success');
+            }
+
+            ob_end_clean();
+            exit($data['m_orderid'] . '|error');
+        }
+
         return Output()
             ->addView(StatusSuccess::class, [
                 'status' => 'Status'

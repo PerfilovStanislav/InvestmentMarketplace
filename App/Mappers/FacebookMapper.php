20e6de83a288c389cd46a37941a78b5bbe2c72fb
stas <stanislav.perfilov@gmail.com>
2021-06-13 14:47:43 +0300

    fix

--- App/Mappers/FacebookMapper.php
+++ App/Mappers/FacebookMapper.php
@@ -9,7 +9,7 @@ class FacebookMapper
     public static function getCollection(): array
     {
         return [
-            Language::EN => \Config::FB_PAGE_EN_ID,
+//            Language::EN => \Config::FB_PAGE_EN_ID,
             Language::RU => \Config::FB_PAGE_RU_ID,
             Language::ZH => \Config::FB_PAGE_ZH_ID,
             Language::HI => \Config::FB_PAGE_HI_ID,
@@ -25,7 +25,7 @@ class FacebookMapper
 
     public static function getPageToken(int $pageId): string {
         return [
-            \Config::FB_PAGE_EN_ID => \Config::FB_PAGE_EN_TOKEN,
+//            \Config::FB_PAGE_EN_ID => \Config::FB_PAGE_EN_TOKEN,
             \Config::FB_PAGE_RU_ID => \Config::FB_PAGE_RU_TOKEN,
             \Config::FB_PAGE_ZH_ID => \Config::FB_PAGE_ZH_TOKEN,
             \Config::FB_PAGE_HI_ID => \Config::FB_PAGE_HI_TOKEN,
